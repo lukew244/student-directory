c3e4c16e7adb26ed77f0209e6b8151bbc648201a
Luke <luke.webster86@gmail.com>
2016-06-21 19:09:41 +0000

    Made the program interactive by taking user input

--- directory.rb
+++ directory.rb
@@ -1,18 +1,19 @@
+# collects names of students from the user and generates an array of hashes containing student data
+def input_students
+	puts "Please enter the name of the students"
+	puts "Hit return twice to finish"
+ 
+	students = []
+	name = gets.chomp
+	
+	while !name.empty? do
+	students << {name: name, cohort: :november}
+	puts "Now we have #{students.count} students"
+	name = gets.chomp
+	end
 
-# array of students 
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+	return students
+end
 
 def print_header
 	puts "The students of Villains Academy"
@@ -29,6 +30,7 @@ def print_footer(students)
 	puts "Overall we have #{students.count} great students\n"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
