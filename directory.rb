208b59313232d605dc9dbe01c492f16ef94ba528
Luke <luke.webster86@gmail.com>
2016-06-25 17:01:02 +0000

    Option to load from students.csv

--- directory.rb
+++ directory.rb
@@ -12,6 +12,7 @@ def print_menu
 	puts "1. Input the students"
 	puts "2. Display student list" 
 	puts "3. Save list to students.csv"
+	puts "4. Load the list from students.csv"
 	puts "9. Exit"
 end
 
@@ -23,6 +24,8 @@ def process(selection)
 		show_students
 	when "3"
 		save_students
+	when "4" 
+		load_students
 	when "9"
 		exit		
 	else 
@@ -109,6 +112,16 @@ def save_students
 	file.close
 end
 
+def load_students
+	file = File.open("students.csv","r")
+	
+	file.readlines.each do |line|
+	name, cohort = line.chomp.split(",")
+	@students << {name: name, cohort: cohort.to_sym}
+	end
+
+	file.close
+end
 
 
 interactive_menu
